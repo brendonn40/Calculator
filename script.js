5b12d8a1005e52c4fc9a60ff223c24ec1d81723c
Brendon-note <brendoncesa40@gmail.com>
2022-07-28 22:04:25 -0300

    estrutura + funcoes basicas

---
+++ script.js
@@ -0,0 +1,38 @@
+const add = function(x,y) {
+	return x+y
+};
+
+const subtract = function(x,y) {
+	return x-y
+};
+
+const sum = function(array) {
+	result = 0
+  for (const i of array) {
+    result+=i
+  }
+  return result
+}
+  
+
+
+const multiply = function(array) {
+  result = 1
+  for (const i of array) {
+    result*=i
+  }
+  return result
+};
+
+const power = function(b,e) {
+	return b ** e
+};
+
+const factorial = function(number) {
+	if(number === 0 ){
+    return 1
+  }
+  else{
+    return number * factorial(number -1)
+  }
+};
