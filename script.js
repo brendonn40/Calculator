b83c78dfaef9f64a0db716faa33b92e0420e4150
Brendon <brendoncesar40@gmail.com>
2022-07-29 23:00:15 -0300

    nao funciona 2 operacoes seguidas

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const add = (x,y) => x+y
 const subtract =(x,y) => x-y
 const divide = (x,y) => x/y
-const mdivtiply =(x,y) => x*y
+const multiply =(x,y) => x*y
 
 const operate = function(operator,x,y) {
     if(operator === "+"){
@@ -11,7 +11,7 @@ const operate = function(operator,x,y) {
         return subtract(x,y)
     }
     if(operator === "*"){
-        return mdivtiply(x,y)
+        return multiply(x,y)
     }
     if(operator === "/"){
         return divide(x,y)
@@ -37,10 +37,25 @@ const buttons = document.querySelectorAll("button")
 
 createListeners()
 
+let number1 = null
+let number2 = null
 function createListeners(){
     buttons.forEach((button) => {
         button.addEventListener("click",function(e){
             e.stopPropagation()
+            if(button.classList.contains("operator")){
+                if(number1 === null){
+                    number1 = parseInt(display.textContent)
+            }else{
+                number2 = parseInt(display.textContent.split(button.id)[1])
+            }
+            }
+            if(number2 !== null){
+                number1 = operate(button.id,number1,number2)
+                number2 = null
+                display.innerText= ""
+                putThingsDisplay(number1)
+            }
             putThingsDisplay(button.id)
         })
     })
@@ -49,3 +64,4 @@ function createListeners(){
 function putThingsDisplay(buttonId){
     display.innerText+=buttonId
 }
+
